8152a43303885f07dc41d342347f7f30478207bb
Joel Silva <joelcsilva@outlook.com>
2018-03-17 18:14:53 -0300

    Finalizando conceitos code-coverage

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -32,6 +32,9 @@ describe('FizzBuzz', () => {
 
   it('should return the number when non-multiple', () =>{
     expect(FizzBuzz(7)).to.be.equal(7);
-
   })
+
+  it('should return 0 when 0', () =>{
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
 });
